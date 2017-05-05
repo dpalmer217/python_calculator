ee96cc94549aa17d4e5229eba73c83dfc6f91d38
David Palmer <palmerdavid217@gmail.com>
2017-05-05 12:57:11 -0700

    Looped redo func

--- calculator.py
+++ calculator.py
@@ -25,11 +25,23 @@ def numselect(operation):
     return (num1, num2)
 
 def redo():
-    redoans = raw_input("Would you like to peform another operation? Y/N \n>")
-    if redoans == "Y":
-        i = 1
-    else:
-        i = 2
+
+    looper = 1
+
+    while looper = 1:
+        redoans = raw_input("Would you like to peform another operation? Y/N \n>")
+
+        if redoans == "Y" or "y":
+            i = 1
+            looper = 2
+
+        elif redoans == "N" or "n":
+            i = 2
+            looper = 2
+
+        else:
+            print "Please select either Y or N"
+
     return i
 
 print "Welcome to my Python Calculator!"
@@ -45,3 +57,20 @@ while i == 1:
         answernum = mult(num1, num2)
         print "The product of %i and %i is %i" % (num1, num2, answernum)
         i = redo()
+
+    elif operation == "Divide" or operation == "divide":
+        (num1, num2) = numselect(operation)
+        answernum = div(num1, num2)
+        print "The quotient of %i and %i is %i" % (num1, num2, answernum)
+
+    elif operation == "Add" or operation == "add":
+        (num1, num2) = numselect(operation)
+        ansernum = add(num1, num2)
+        print "The sum of %i and %i is %i" % (num1, num2, answernum)
+
+    elif operation == "Subtract" or operation == "subtract":
+        (num1, num2) = numselect(operation)
+        answernum = sub(num1, num2)
+
+    else:
+        print "Please choose either Add, Subtract, Multiply, or Divide"
